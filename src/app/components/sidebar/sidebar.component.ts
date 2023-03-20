d0c43b51c04cda43756955ec9e7c23ef6376aa2b
dluca22 <luc.due@tim.it>
2023-03-20 11:48:24 +0100

    changed http request to an observable test the difference?

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { ListService } from 'src/app/services/list.service';
 import { Machine } from 'src/app/Machine';
 
 // this is the sidebar in the main part of the page, where I think i want to add the 3 lists of sections of the page
 // right now it will be fetching the data from "/machines" endpoint, getting back all the entries in the `machines` table of the database, via the `/machines` endpoint from Node
 
-// uses ListService.ts as a service to fetch to the backend 
+// uses ListService.ts as a service to fetch to the backend
 
 
 @Component({
@@ -14,21 +15,14 @@ import { Machine } from 'src/app/Machine';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent  implements OnInit {
-  machines: Machine[] = [];
+  machines!: Observable<Machine[]>;
 
   constructor(private listService: ListService) {}
 
   ngOnInit(): void {
-    console.log("init machine list")
     // on initializing calls this listService method imported from list.service
-    this.listService.getMachineList().subscribe( data => {
-      console.log("qua i dati", data);
-      this.machines = data;
-    },
-    error => {
-      console.log("errore", error);
-
-    } )
+    this.machines = this.listService.getMachineList()
+    console.log("qui!", this.machines)
   }
 
 }
